af4c717872d7531a6a45cbbdc8fb483d9d8a7e13
pereirad <pereirad@usf.edu>
2022-02-24 22:04:10 -0500

    Initial check-in of module T4_Kattis_Bela

---
+++ T4_Kattis_Bela/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace T4_Kattis_Bela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the number of hands in the game");
+
+            Console.Write("Please enter the dominant suit (S,H,D,C) in the game");
+        }
+    }
+}
